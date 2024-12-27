24da3978e56b4f9ed2780aa5c8cccd21d03f6dd0
MohyJake21 <mohanrajib21@gmail.com>
2024-12-27 16:04:24 +0530

    Update Player.jsx

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="" />
+      <iframe src='https://www.youtube.com/embed/QOhj3m0fHnA' width='90%' height='90%' title='trailer' 
+      frameBorder='0' allowFullScreen></iframe>
+
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
     </div>
   )
 }
