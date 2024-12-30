20538d8d801b1664d32678fb38d03e1fac15881a
MohyJake21 <mohanrajib21@gmail.com>
2024-12-30 15:10:09 +0530

    firebase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,24 @@
 import React, {useState} from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { login, signup,  } from '../../firebase'
 
 const Login = () => {
 
-  const [signState, setSignState] = useState("Sign In")
+  const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const user_auth = async (event) => {
+    event.preventDefault();
+    if(signState==="Sign In"){
+      await login(email, password);
+    }
+    else{
+      await signup(name, email, password);
+    }
+  }
 
   return (
     <div className='login'>
@@ -12,11 +26,16 @@ const Login = () => {
       <div className='login-form'>
           <h1>{signState}</h1>
           <form>
-            {signState==="Sign Up"?<input type="text" name="" id="" placeholder='Your Name'/>:<></>}
+            {signState==="Sign Up"?
+            <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}
+             name="" id="" placeholder='Your Name'/>:<></>}
 
-            <input type="email" name="" id="" placeholder='Email'/>
-            <input type="password" name="" id="" placeholder='Password'/>
-            <button>{signState}</button>
+            <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}
+             name="" id="" placeholder='Email'/>
+
+            <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}
+            name="" id="" placeholder='Password'/>
+            <button onClick={user_auth} type='submit'>{signState}</button>
 
             <div className="form-help">
               <div className="remember">
